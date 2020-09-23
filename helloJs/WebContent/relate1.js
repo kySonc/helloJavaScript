ab2056f72cf3c0ac66988408ef5dfa1580a9e29f
kySon <dlswhsqhr@gmail.com>
2020-09-23 11:28:30 +0900

    내포 관계인 자바스크립트 파일 참조하기

---
+++ helloJs/WebContent/relate1.js
@@ -0,0 +1,3 @@
+document.write("relate1.js는 relat.js에 포함");
+document.write("<div style='color:blue; fonte-size:20px;'>외부 자바스크립트 파일2</div>");
+document.write("<script src='relate2.js'></script>");
---
+++ helloJs/WebContent/relate2.js
@@ -0,0 +1,3 @@
+document.write("relate2.js는 relate1.js에 포함");
+document.write("<div style='color:green; fonte-size:16px;'>외부 자바스크립트 파일3</div>");
+alert('Nested Script File');
